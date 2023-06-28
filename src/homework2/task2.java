a9d4d64cd614d1118593f398fb89d3d5a4e2dc5a
OlgaP <ooolchaaa@inbox.ru>
2023-06-28 10:10:34 +0300

    add source code to task 2 and 3

--- src/homework2/task2.java
+++ src/homework2/task2.java
@@ -6,3 +6,13 @@ public class task2 {
         System.out.println("catchedRes1 = " + catchedRes1);
     }
 }
+
+// Исходный код
+// try {
+//    int d = 0;
+//    double catchedRes1 = intArray[8] / d;
+//    System.out.println("catchedRes1 = " + catchedRes1);
+// } catch (ArithmeticException e) {
+//    System.out.println("Catching exception: " + e);
+// }
+
